0b82f3d934bb245d437e83a7e9447a157bed2ab7
Nikorambus <nick-mod@mail.ru>
2023-11-10 18:01:32 +0300

    first

--- 201_331_Egel/201_331_Egel.pro
+++ 201_331_Egel/201_331_Egel.pro
@@ -4,6 +4,11 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 CONFIG += c++17
 
+INCLUDEPATH += D:\Qt\Tools\OpenSSL\Win_x64\include
+
+LIBS += D:/Qt/Tools/OpenSSL/Win_x64/lib/libcrypto.lib
+LIBS += D:/Qt/Tools/OpenSSL/Win_x64/lib/libssl.lib
+
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
--- 201_331_Egel/mainwindow.h
+++ 201_331_Egel/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "QSignalMapper"
+#include "QListWidgetItem"
+#include "QVBoxLayout"
+#include "QPushButton"
+#include "QMessageBox"
+#include "QLabel"
+#include "QLineEdit"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +22,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    //Переменные для окна ввода пин кода
+    QWidget *pin_window;
+    QLineEdit *input_pincode;
+    void show_pin_check_window();//Функция показа окна для ввода pincode
+    unsigned char *key = (unsigned char *)"81dc9bdb52d04dc20036dbd8313ed055";
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+    QString decrypt_file(QByteArray encrypt_data);
+    int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,unsigned char *iv, unsigned char *decryptext);
+    int crypt_error(void);
+    QVector<QVector<QString>> list_of_account;
+
+    int show_bank_window();
+private slots:
+    int check_pin_code();
+
+
 
 private:
     Ui::MainWindow *ui;
